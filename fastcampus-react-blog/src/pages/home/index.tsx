cb128d8f5d417df3f779bbb795a9d3ff5f4053bf
minhee <jmh3699@naver.com>
2024-05-13 16:33:52 +0900

    [블로그] 홈 컴포넌트 작업

--- fastcampus-react-blog/src/pages/home/index.tsx
+++ fastcampus-react-blog/src/pages/home/index.tsx
@@ -10,11 +10,37 @@ export default function Home() {
 					<Link to="/profile">프로필</Link>
 				</div>
 			</header>
-			<div className="post-list">Post List</div>
+			<div className="post__navigation">
+				<div className="post__navigation--active">전체</div>
+				<div>나의 글</div>
+			</div>
+			<div className="post__list">
+				{[...Array(10)].map((_, index) => (
+					<div key={index} className="post__box">
+						<Link to={`/posts/${index}`}>
+							<div className="post__profile-box">
+								<div className="post__profile" />
+								<div className="post__author-name">패스트캠퍼스</div>
+								<div className="post__date">2024.05.13</div>
+							</div>
+							<div className="post__title">게시글 {index}</div>
+							<div className="post__text">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni expedita quasi
+								praesentium non saepe nostrum explicabo debitis rerum quibusdam quia sequi, laborum
+								eligendi amet architecto vero doloribus hic aliquam dolores.
+							</div>
+							<div className="post__util-box">
+								<div className="post__delete">삭제</div>
+								<div className="post__edit">수정</div>
+							</div>
+						</Link>
+					</div>
+				))}
+			</div>
 			<footer>
-				<div>Menu 1</div>
-				<div>Menu 2</div>
-				<div>Menu 3</div>
+				<Link to="/posts/new">글쓰기</Link>
+				<Link to="/posts">게시글</Link>
+				<Link to="/profile">프로필</Link>
 			</footer>
 		</>
 	);
